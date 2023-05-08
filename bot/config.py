b2bf4f73049421a959133ad6e07ab2e7261b9307
lyft168 <132919872+lyft168@users.noreply.github.com>
2023-05-08 22:30:00 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,8 +13,8 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
 telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
-use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
+#openai_api_key = config_yaml["openai_api_key"]
+#use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
@@ -22,6 +22,11 @@ return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 
+
+# apis
+with open(config_dir / "api.yml", 'r') as f:
+    api = yaml.safe_load(f)
+    
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
     chat_modes = yaml.safe_load(f)
